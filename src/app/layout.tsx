23b8624a7b58b48e8d4b3bc0309be749e8ca6e18
doudouman01 <goutercalaydou@gmail.com>
2026-09-25 15:32:03 -0700

    update 25/09/2026 15:32

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,9 @@ export const metadata: Metadata = {
     type: "website",
   },
   robots: { index: true, follow: true },
+  verification: {
+    google: 'YccvnsA5nHRNIbJ7eqcqGXq0O6Qubsr9s4JXaDBL3J4',
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
